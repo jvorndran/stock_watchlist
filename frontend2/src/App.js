8ec66ea17c79e5d9067890ee9f613402c3f58645
jvorndran <135787855+jvorndran@users.noreply.github.com>
2023-09-20 11:43:52 -0400

    Organizing code

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -4,16 +4,13 @@ import Login from "./features/auth/Login";
 import Signup from "./features/auth/Signup";
 import DashLayout from "./components/Dashboard/DashLayout";
 import StockPage from "./components/StockPage/StockPage";
-import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Layout />}>
 
-          <Route index element={<LandingPage />} />
-
-          <Route path="login" element={<Login />} />
+          <Route path="" element={<Login />} />
 
           <Route path="signup" element={<Signup />} />
 
@@ -21,11 +18,8 @@ function App() {
 
           <Route path="dash/:stock_ticker" element={<StockPage />} />
 
-            <Route path="landing" element={<LandingPage />} />
-
         </Route>
       </Routes>
   );
 }
-
 export default App;
